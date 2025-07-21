1e6c8a17a437db3ef5d2eea247afdc7a5e66f0b1
Vinu Joseph <vinujoseph25@gmail.com>
2025-07-21 17:11:53 +0530

    changes

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -12,11 +12,11 @@ interface LayoutProps {
 
 export default function Layout({ headerName = '', children }: LayoutProps) {
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ position: 'relative' }}>
       <div className="side-menu">
         <Menu />
       </div>
-      <div className="content" style={{ flex: 1 }}>
+      <div className="content">
         <Header />
         <Body>{children}</Body>
         <Footer />
